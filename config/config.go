3b6ed9c8554b2220cb675866699ca51eb70b29b2
imegao <44913208+imegao@users.noreply.github.com>
2020-02-18 14:25:08 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -16,7 +16,7 @@ type Config struct {
 	DbConnMaxLifeSeconds   int           `toml:"db_conn_max_life_seconds"`
 	ApiIdUrl               string        `toml:"api_id_url"`
 	ApiScrollUrl           string        `toml:"api_scroll_url"`
-	FileSizeLimit          int64           `toml:"file_size_limit"`
+	FileSizeLimit          int64         `toml:"file_size_limit"`
 	Producer               DummyProducer `toml:"producer"`
 }
 
