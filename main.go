18e5918fa0cb7ef6d8a67b3879c8e2772aff899a
imegao <843342058@qq.com>
2020-02-19 14:05:18 +0800

    change timestamp format

--- main.go
+++ main.go
@@ -112,7 +112,7 @@ func HourTimestamp() (string, string, string) {
 	h2, _ := time.ParseDuration("-2h")
 	endTime := now.Add(h1).Format("2006-01-02 15:04:05")
 	startTime := now.Add(h2).Format("2006-01-02 15:04:05")
-	lastTime:=startTime
+	lastTime := now.Add(h2).Format("2006-01-02-15-04-05")
 	return startTime, endTime, lastTime
 }
 
